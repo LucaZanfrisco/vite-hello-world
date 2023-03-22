53b7b2404d39f7f96e13ad0ba82e3d2d2e0f4204
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-03-22 16:16:46 +0100

    Componente AppHero per il jumbotron

---
+++ src/components/AppHero.vue
@@ -0,0 +1,26 @@
+<script>
+    export default{
+        data(){
+            return{
+                jambotron: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Marvel_Studios_logo.svg/2560px-Marvel_Studios_logo.svg.png',
+            }
+        }
+    };
+</script>
+<template>
+    <div class="jumbotron">
+        <img :src="jambotron" alt="jambotron">
+    </div>
+</template>
+<style scoped>
+    .jumbotron{
+        width: 100%;
+        height: 31.25rem;
+        background-color: #d40317;
+    }
+    img{
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
+</style>
--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -6,9 +6,11 @@
     <div>
         <h1>La mia prima app con Vite!</h1>
     </div>
-
 </template>
 
 <style scoped>
-
+    h1{
+        text-align: center;
+        padding: 30px;
+    }
 </style>
