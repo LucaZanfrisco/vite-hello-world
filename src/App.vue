9a9bf4f13e9f888051c3defe0b3714c1e2219e56
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-03-22 15:39:39 +0100

    Primo componente

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
-<script setup>
+<script>
+  import AppTitle from './components/AppTitle.vue';
+
+  export default {
+    components:{
+      AppTitle,
+    }
+  }
 </script>
 
 <template>
-  
+    <AppTitle></AppTitle>
 </template>
 
 <style scoped>
